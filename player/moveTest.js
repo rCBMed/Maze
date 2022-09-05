77d709a754e5758d22437e7e071aeb4f70e1a5fa
Ruchi <ruchi06.cbm@gmail.com>
2022-09-05 11:28:38 +0530

    commit message

--- player/moveTest.js
+++ player/moveTest.js
@@ -1,7 +1,7 @@
 
-var imageRepo= new function(){
+var imageRepo = new function(){
 
-  this.img =new Image();
+this.img =new Image();
 this.img.src='https://opengameart.org/sites/default/files/Green-Cap-Character-16x18.png';
 
 this.bg =new Image();
@@ -31,6 +31,42 @@ this.spawn=function(x,y,speed){
   this.ctx.drawImage(imageRepo.img, this.x.this.y)
 }
 
+//Sprite start position
+function playerMovement(){
+  this.speed = 1;
+  this.step=1;
+  this.x=160;
+  this.y=35;
+  this.draw = function(){
+  this.ctx.drawImage(imageRepo.player, this.x, this,y);
+  };
+  this.move = function(){
+    counter++;
+    // check movement commands
+    if(KEY_STATUS.left || KEY_STATUS.right || KEY_STATUS.down || KEY_STATUS.UP){
+      this.ctx.clearRect(this.x, this.y, this.width, this.height);
+      if(KEY_status.left){
+        this.x-=this.speed
+        if (this.x <=0)
+        this.x=0;
+      }else if(KEY_STATUS.right){
+        this.x += this.speed
+        //EXPLAIN?
+        if(this.x >= this.canvasWidth - this.width)
+        this.x = this.canvasWidth - this.width;
+      }else if (KEY_status.up){
+        this.y-=this.speed
+        if(this.y <= this.canvasHeight/4*3)
+        this.y=this.canvasHeight/4*3;
+      }else if (KEY_STATUS.down){
+        this.y+=this.speed
+        if(this.y >= this.canvasHeight -this.Height)
+        this.y >= this.canvasHeight - this.height;
+      }
+      this.draw();
+    }
+  }
+}
 
 
 function background(){
@@ -46,16 +82,3 @@ function init(){
 }
 
   
-/*
-function playerMovement(){
-
-    this.move = function(){
-      counter++;
-      //determine action from move
-      if(KEY_STATUS.left || KEY_STATUS.right || KEY_STATUS.down || KEY_STATUS.up){
-        this.context.clearRect(this.x, this.y, this.width, this.height);
-        
-      }
-    }
-}
-*/
